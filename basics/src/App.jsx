227b408c3515bd554b7f5b4c6477f42a3e365beb
pranay-w <pranaywabhitkar6429@gmail.com>
2026-04-13 20:45:58 +0530

    added basics

---
+++ basics/src/App.jsx
@@ -0,0 +1,16 @@
+
+
+function App() {
+  
+  let name="Pranay"
+  return (
+    <>
+    <h1>Hello, Everyone !</h1>
+    <p>I am {name} , from Btech CSE - AI/ML</p>
+    <p>This is React js Repo</p>
+    <p>In this Repo you will get, A structured and beginner-to-advanced learning repository for <br /> React.js, built using Vite. Covers fundamentals, core concepts, components, hooks, and real-world examples with <br /> daily updates.</p>
+    </>
+  )
+}
+
+export default App
